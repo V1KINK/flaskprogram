5b542a534b3f73c645c987a537260fd0b235749d
王凯 <Wk1370654867@163.com>
2019-06-09 15:49:56 +0800

    创建info包&config.py

--- manager.py
+++ manager.py
@@ -3,14 +3,14 @@ from flask import Flask, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
-# from flask_script import Manager
+from flask_script import Manager
 from redis import StrictRedis
 
 
 class Config(object):
     DEBUG = True
 
-    SECRET_KEY = "afdgdgdfhadfgjh"
+    SECRET_KEY = "KrVXEGfe+i8nHfkESPCcY8EVdorXrWYrUXnousUiibB26IgOKiDiS8i8KDxIhBZb"
 
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flaskprogram"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,7 +18,7 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
-    SECRET_TYPE = "redis"
+    SESSION_TYPE = "redis"
     SESSION_USE_SIGNER = True
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     SESSION_PERMANENT = False
@@ -31,6 +31,7 @@ db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 Session(app)
+Manager(app)
 
 
 @app.route("/")
