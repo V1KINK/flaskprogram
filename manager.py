04f82f32de06879752a28a30293bc117f9b7c65f
王凯 <Wk1370654867@163.com>
2019-06-09 11:59:25 +0800

    立项

--- manager.py
+++ manager.py
@@ -1,7 +1,31 @@
+import redis
 from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_script import Manager
 
 
 app = Flask(__name__)
+
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flaskprogram"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    SECRET_KEY = ""
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
+manager = Manager(app)
 
 
 @app.route("/")
@@ -10,4 +34,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
