2c12a3eecf9062feff745cabcaeaea903589f47f
王凯 <Wk1370654867@163.com>
2019-06-11 20:50:19 +0800

    根路径模板渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,10 @@
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
 
 @index_blu.route("/")
 def index():
-    redis_store.set("name", "itcast")
-    return "index"
+    # redis_store.set("name", "itcast")
+    return render_template("news/index.html")
