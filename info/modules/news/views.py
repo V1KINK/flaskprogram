4223c2284896edaf78b88ef24120b34b57966b47
王凯 <Wk1370654867@163.com>
2019-06-15 18:43:15 +0800

    新闻详情页基本模板数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,10 @@
+from flask import current_app
 from flask import render_template
 from flask import request, jsonify
+from flask import session
 
+from info import constants
+from info.models import News, Category, User
 from info.utils.response_code import RET
 from . import news_blu
 
@@ -8,7 +12,35 @@ from . import news_blu
 # 新闻模板渲染
 @news_blu.route("/<int:news_id>")
 def news_detail(news_id):
-    # news_id = request.json.get("news_id")
-    # if not news_id:
-    #     return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
-    return render_template("news/detail.html")
+    # 查询用户登陆信息
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    # 右侧的新闻排行逻辑
+    news_clicks_list = []
+    try:
+        news_clicks_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_li = []
+    for news in news_clicks_list:
+        news_dict_li.append(news.to_basic_dict())
+
+    categories = Category.query.all()
+    category_list = []
+    for category in categories:
+        category_list.append(category.to_dict())
+
+    data = {
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li
+    }
+
+    return render_template("news/detail.html", data=data)
