07a7c5f2514cbfe79c531aabd18ea675687e54a5
王凯 <Wk1370654867@163.com>
2019-06-15 17:30:11 +0800

    首页新闻详情页面模板展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,14 @@
-# @news_blu.route("/news_collect", methods=["POST"])
-# def
-#
-#     news_id =request.josn.ger("news_id")
-#     if not news_id:
-#         return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+from flask import render_template
+from flask import request, jsonify
+
+from info.utils.response_code import RET
+from . import news_blu
+
+
+# 新闻模板渲染
+@news_blu.route("/<int:news_id>")
+def news_detail(news_id):
+    # news_id = request.json.get("news_id")
+    # if not news_id:
+    #     return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    return render_template("news/detail.html")
