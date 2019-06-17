fb0dca01f0cefc91b0e03fb48c0053403f2cddd4
王凯 <Wk1370654867@163.com>
2019-06-17 14:51:55 +0800

    基本资料显示

--- info/modules/profiles/views.py
+++ info/modules/profiles/views.py
@@ -1,9 +1,19 @@
-from flask import g
+from flask import g, jsonify
 from flask import redirect
 from flask import render_template
+from flask import request
 
 from info.modules.profiles import profiles_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@profiles_blu.route("/base_info", methods=["GET", "POST"])
+@user_login_data
+def base_info():
+
+    if request.method == "GET":
+        return render_template("news/user_base_info.html", data={"user": g.user.to_dict()})
 
 
 @profiles_blu.route("/user_info")
