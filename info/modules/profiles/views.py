3a07924b70a5bf4817414613858e9e59dcd7fa45
王凯 <Wk1370654867@163.com>
2019-06-17 19:53:47 +0800

    基本资料修改

--- info/modules/profiles/views.py
+++ info/modules/profiles/views.py
@@ -11,9 +11,27 @@ from info.utils.response_code import RET
 @profiles_blu.route("/base_info", methods=["GET", "POST"])
 @user_login_data
 def base_info():
-
+    # get 请求方式
     if request.method == "GET":
         return render_template("news/user_base_info.html", data={"user": g.user.to_dict()})
+
+    # 修改用户信息
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERRK, errmsg="参数错误")
+
+    user = g.user
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profiles_blu.route("/user_info")
