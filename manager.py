9cbe148b1f88e5d2f4053a41d6be34233208fbaa
王凯 <Wk1370654867@163.com>
2019-06-20 16:14:55 +0800

    后台管理员程序集成

--- manager.py
+++ manager.py
@@ -1,6 +1,10 @@
+from flask import current_app
+from flask import jsonify
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db, models
+from info.models import User
+from info.utils.response_code import RET
 
 app = create_app("development")
 manager = Manager(app)
@@ -8,5 +12,28 @@ Migrate(app, db)
 manager.add_command("db", MigrateCommand)
 
 
+@manager.option("-n", "-name", dest="name")
+@manager.option("-p", "-password", dest="password")
+def create_admin(name, password):
+    if not all([name, password]):
+        print("参数不全")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        print(e)
+
+    print("添加成功")
+
+
 if __name__ == '__main__':
+    print(app.url_map)
     manager.run()
